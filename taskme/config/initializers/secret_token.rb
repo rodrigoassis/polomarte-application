0c16df9662bb5bf0888354d6e411ec9cf413ff20
Rodrigo Assis <rodrigo.assis.fialho@gmail.com>
2012-11-23 08:44:21 -0200

    primeiro commit

---
+++ taskme/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskme::Application.config.secret_token = 'adcf6a469b09c9a1d8ba42b97a214d480e669dd91e8d326e217a3f16771f83384815c3ccc54488c8237c29eaf8d98f08440586e49fdee4ce4d19628551e80c4e'
