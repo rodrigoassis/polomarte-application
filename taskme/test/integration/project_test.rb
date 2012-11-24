2e1bb2f307579e043c6d62c1b7073c9135209794
Rodrigo Assis <rodrigo.assis.fialho@gmail.com>
2012-11-24 19:41:15 -0200

    notices

--- taskme/test/integration/project_test.rb
+++ taskme/test/integration/project_test.rb
@@ -3,4 +3,40 @@ require 'test_helper'
 class ProjectTest < ActionDispatch::IntegrationTest
   fixtures :all
 
+	test 'root' do
+		visit('/')
+		assert_equal root_path, current_path
+	end
+
+	test 'new vai pro form' do
+		visit('/')
+		click_link('New')
+
+		assert_equal new_project_path, current_path
+	end
+
+	test 'form possui campo name' do
+		visit(new_project_path)
+
+		assert find_field('Name')
+		assert !page.has_field?('Finished')
+	end
+
+	test 'name cant be blank' do
+		visit(new_project_path)
+
+		click_button('Create Project')
+		assert page.has_content?("can't be blank")
+	end
+
+	test 'create project' do
+		visit(new_project_path)
+
+		fill_in('Name', :with => 'Test')
+		click_button('Create Project')
+
+		assert_equal projects_path, current_path
+		assert page.has_content?('Project was successfully created.')
+	end
+
 end
